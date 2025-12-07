6e9d823b3aa7d8eeb1957821ed782d44b540360c
yiwen985 <yiwen985@gmail.com>
2025-12-07 22:52:26 +0800

    fix bookmark

--- scripts/bookmark.lua
+++ scripts/bookmark.lua
@@ -66,6 +66,9 @@ local function bookmark_add()
         prompt = "添加书签（时间 标题）: ",
         submit = function(text)
             local line = tool.trim(text)
+            if line == "" then
+                return
+            end
             local item = tool.parse_line(line)
             if item then
                 table.insert(chapters, item)
@@ -119,12 +122,18 @@ local function bookmark_remove(remember_pos)
 end
 
 
-local function bookmark_modify()
+local function bookmark_modify(default_index)
     if not next(chapters) then
         mp.osd_message("No available chapters.")
         return
     end
-    local default_item = mp.get_property_native("chapter")
+    local default_item
+    if default_index then
+        default_item = default_index
+    else
+        default_item = mp.get_property_native("chapter")
+        default_item = default_item > -1 and default_item + 1
+    end
 
     local chps = {}
     for i, item in ipairs(chapters) do
@@ -134,7 +143,7 @@ local function bookmark_modify()
     input.select({
         prompt = "修改书签:",
         items = chps,
-        default_item = default_item > -1 and default_item + 1,
+        default_item = default_item,
         submit = function(option_num)
             -- print("option_num " .. option_num) -- 没有这行，下方无法打印 text
             mp.add_timeout(0.01, function() -- 确保 mpv 有时间处理上一个菜单的关闭，并准备好显示新菜单. 0.01 比 0 好，这样可以注释上面一行
@@ -143,6 +152,14 @@ local function bookmark_modify()
                     submit = function(text)
                         print(text)
                         local line = tool.trim(text)
+                        if line == "" then
+                            table.remove(chapters, option_num)
+                            updateChapterList()
+                            mp.add_timeout(0, function()
+                                bookmark_modify(option_num)
+                            end)
+                            return
+                        end
                         local item = tool.parse_line(line)
                         local _, original_item = next(chapters, option_num)
                         if original_item and original_item.time == item.time then
@@ -158,7 +175,9 @@ local function bookmark_modify()
                         end
                         updateChapterList()
 
-                        mp.add_timeout(0, bookmark_modify)
+                        mp.add_timeout(0, function()
+                            bookmark_modify(option_num)
+                        end)
                     end,
                     default_text = chps[option_num] .. " "
                 })
@@ -219,6 +238,11 @@ local function bookmark_add_append()
         prompt = "添加加书签（时间 标题）: ",
         submit = function(text)
             local line = tool.trim(text)
+            if line == "" then
+                table.remove(chapters, pos)
+                updateChapterList()
+                return
+            end
             local item = tool.parse_line(line)
             if item then
                 table.remove(chapters, pos)
